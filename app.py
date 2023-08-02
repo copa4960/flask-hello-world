9ba21b118f21569df51c2ef91456c42b1eadd75f
copa4960 <copa4960@colorado.edu>
2023-08-02 00:06:08 +0000

    first

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def hello_word():
+    conn = psycopg2.connect("postgres://maxdata_user:37E7NQuXm9T45FSPJbvyI9SQ1jOk8z1a@dpg-cj4pptgeba7s73a91e80-a/maxdata")
+    conn.close()
+    return 'database connection successful'
